4f8cda0170af3f8ee37a78fad40c625064567f8d
Luiz Henrique Burgos <henriquerburgos@gmail.com>
2022-10-15 00:43:42 -0300

    Implementação do Service API e atualização do formulario.

---
+++ src/app/Shared/models/api-response.ts
@@ -0,0 +1,5 @@
+export interface ApiResponse {
+    cotacaoCompra: number,
+      cotacaoVenda: number,
+      dataHoraCotacao: string
+    }
--- src/app/quote-history/quote-history.component.ts
+++ src/app/quote-history/quote-history.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { NgbCalendar, NgbDate, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { Dropdown } from '../Shared/models/dropdown';
+import * as moment from 'moment' ;
+import { ApiRequest } from '../Shared/models/api-request';
+import { ApiService } from '../Shared/Services/api.service';
+import { ApiResponse } from '../Shared/models/api-response';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-quote-history',
@@ -9,7 +13,10 @@ import { Dropdown } from '../Shared/models/dropdown';
   styleUrls: ['./quote-history.component.css']
 })
 export class QuoteHistoryComponent implements OnInit {
- 
+  today!:Date
+  formulario!:ApiRequest
+  apiResponse!:Observable<ApiResponse[]>
+
   search = this.formBuilder.group({
     moeda: ['', Validators.required],
     dataInicial: ['', Validators.required],
@@ -35,15 +42,26 @@ export class QuoteHistoryComponent implements OnInit {
     }
   ]
 
-sendDatas(){
-  let formulario ={ ... this.search.value}
-  return console.log(formulario)
+sendData(){
+  let dataInicial= moment(this.search.get('dataInicial')?.value).format('MM-DD-YYYY')
+  let dataFinal= moment(this.search.get('dataFinal')?.value).format('MM-DD-YYYY')
+  let moeda=this.search.get('moeda')?.value
+
+  this.formulario={ dataInicial,dataFinal,moeda}
+  console.log(this.formulario);
+  
+this.apiResponse=this.api.searchCurrency(this.formulario);
+}
+attData(){
+return this.formulario? console.log(this.api.searchCurrency(this.formulario).subscribe(a=>console.log(a)
+)): alert("o formulario está vazio");
 }
 
-  constructor(private formBuilder: FormBuilder) {  }
+  constructor(private formBuilder: FormBuilder, private api:ApiService) {  }
 
   ngOnInit(): void {
-    this.search.get("dataInicial")?.value
+    this.today=new Date()
+    
   }
 
 }
